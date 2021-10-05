f7a6c756f638e46302e43dfef34fb812ef8cd4f6
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-05 14:32:42 -0500

    create get balance gets balance method and test to get bankaccount balance

---
+++ lab02-atm.tests/BankAccountTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace lab02_atm.tests
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void Get_Balance_Gets_Balance()
+        {
+
+            //Arrange
+            BankAccount bank = new BankAccount();
+
+            //Act
+            decimal balance = bank.GetBalance();
+
+            //Assert
+            Assert.Equal(1, balance);
+        }
+    }
+}
---
+++ lab02-atm/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace lab02_atm
+{
+    public class BankAccount
+    {
+        public decimal GetBalance()
+        {
+            return 1;
+        }
+    }
+}
